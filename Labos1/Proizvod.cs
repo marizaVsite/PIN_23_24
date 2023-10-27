98d547675e367aed9b3524423e75cf1d93da15e5
marizaVsite <92469723+marizaVsite@users.noreply.github.com>
2023-10-27 12:28:07 +0200

    Labos1

---
+++ Labos1/Proizvod.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Labos1
+{
+    public class Proizvod
+    {
+        // Svojstva (properties)
+        public string Naziv { get; set; }
+        public decimal Cijena { get; set; }
+        public int DostupnaKolicina { get; set; }
+
+        // Konstruktor za inicijalizaciju objekta
+        public Proizvod(string naziv, decimal cijena, int dostupnaKolicina)
+        {
+            Naziv = naziv;
+            Cijena = cijena;
+            DostupnaKolicina = dostupnaKolicina;
+        }
+
+        // Metoda za ispis informacija o proizvodu
+        public void IspisiInformacije()
+        {
+            Console.WriteLine($"Proizvod: {Naziv}");
+            Console.WriteLine($"Cijena: {Cijena:C}"); // Formatiranje cijene kao valute
+            Console.WriteLine($"Dostupna količina: {DostupnaKolicina}");
+        }
+    }
+}
