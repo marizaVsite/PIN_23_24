fd1fe7933719f0a6d0e0bc4180544ca0c20e94f0
mmaini <mariza.maini@itsistemi.com>
2023-10-27 12:22:57 +0200

    Labos1

---
+++ Labos1/Labos1/Nasljedjivanje.cs
@@ -0,0 +1,45 @@
+﻿namespace Labos1
+{
+    using System;
+
+    // Bazna klasa za elektroničke uređaje
+    public class ElektroničkiUređaj
+    {
+        public string Naziv { get; set; }
+        public string Proizvođač { get; set; }
+        public int GodinaProizvodnje { get; set; }
+
+        public ElektroničkiUređaj(string naziv, string proizvođač, int godinaProizvodnje)
+        {
+            Naziv = naziv;
+            Proizvođač = proizvođač;
+            GodinaProizvodnje = godinaProizvodnje;
+        }
+
+        public void IspisiInformacije()
+        {
+            Console.WriteLine($"Uređaj: {Naziv}");
+            Console.WriteLine($"Proizvođač: {Proizvođač}");
+            Console.WriteLine($"Godina proizvodnje: {GodinaProizvodnje}");
+        }
+    }
+
+    // Podklasa za pametne telefone
+    public class PametniTelefon : ElektroničkiUređaj
+    {
+        public string OperativniSustav { get; set; }
+
+        public PametniTelefon(string naziv, string proizvođač, int godinaProizvodnje, string os)
+            : base(naziv, proizvođač, godinaProizvodnje)
+        {
+            OperativniSustav = os;
+        }
+
+        // Dodatna metoda specifična za pametne telefone
+        public void InstalirajAplikaciju(string aplikacija)
+        {
+            Console.WriteLine($"Instalira se aplikacija: {aplikacija}");
+        }
+    }
+
+}
---
+++ Labos1/Labos1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Labos1
+{
+    public class Program
+    {
+        static void Main()
+        {
+            Proizvod p = new Proizvod("test", (decimal)4.5, 5);
+            p.IspisiInformacije();
+
+            PametniTelefon telefon = new PametniTelefon("iPhone 13", "Apple", 2022, "iOS");
+            telefon.IspisiInformacije();
+            telefon.InstalirajAplikaciju("Facebook");
+
+
+        }
+
+    }
+
+}
