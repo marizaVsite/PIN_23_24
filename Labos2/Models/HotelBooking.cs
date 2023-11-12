44641f791fc6168344499eb7b284093399efd4c1
marizaVsite <92469723+marizaVsite@users.noreply.github.com>
2023-11-12 16:53:46 +0100

    Add files via upload
    Labos2

---
+++ Labos2/Models/HotelBooking.cs
@@ -0,0 +1,9 @@
+﻿namespace Labos2.Models
+{
+    public class HotelBooking
+    {
+        public int Id { get; set; }
+        public string GuestName { get; set; }
+        public int RoomNumber { get; set; }
+    }
+}
